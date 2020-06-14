00619ecaaeb421b2552163313752db78b99f7201
Liron Meler <lironmeler@gmail.com>
2020-06-14 16:11:12 +0300

    Add GUI

---
+++ ImageViewer/MainForm.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace ImageViewer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnChooseFile = new System.Windows.Forms.Button();
+            this.lblChoose = new System.Windows.Forms.Label();
+            this.lblWelcome = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnChooseFile
+            // 
+            this.btnChooseFile.Location = new System.Drawing.Point(9, 130);
+            this.btnChooseFile.Name = "btnChooseFile";
+            this.btnChooseFile.Size = new System.Drawing.Size(322, 85);
+            this.btnChooseFile.TabIndex = 0;
+            this.btnChooseFile.Text = "Choose image file";
+            this.btnChooseFile.UseVisualStyleBackColor = true;
+            this.btnChooseFile.Click += new System.EventHandler(this.btnChooseFile_Click);
+            // 
+            // lblChoose
+            // 
+            this.lblChoose.AutoSize = true;
+            this.lblChoose.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblChoose.Location = new System.Drawing.Point(7, 68);
+            this.lblChoose.Name = "lblChoose";
+            this.lblChoose.Size = new System.Drawing.Size(316, 29);
+            this.lblChoose.TabIndex = 1;
+            this.lblChoose.Text = "Please choose an image file";
+            // 
+            // lblWelcome
+            // 
+            this.lblWelcome.AutoSize = true;
+            this.lblWelcome.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWelcome.Location = new System.Drawing.Point(5, 13);
+            this.lblWelcome.Name = "lblWelcome";
+            this.lblWelcome.Size = new System.Drawing.Size(167, 37);
+            this.lblWelcome.TabIndex = 2;
+            this.lblWelcome.Text = "Welcome!";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(346, 227);
+            this.Controls.Add(this.lblWelcome);
+            this.Controls.Add(this.lblChoose);
+            this.Controls.Add(this.btnChooseFile);
+            this.Name = "MainForm";
+            this.Text = "Image Viewer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnChooseFile;
+        private System.Windows.Forms.Label lblChoose;
+        private System.Windows.Forms.Label lblWelcome;
+    }
+}
+
---
+++ ImageViewer/MainForm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ImageViewer
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        private void btnChooseFile_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
