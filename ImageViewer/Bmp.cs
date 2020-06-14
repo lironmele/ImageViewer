0a8dad7a258725d9ce415decd82da14fdc7d1dd2
Liron Meler <lironmeler@gmail.com>
2020-06-14 19:40:16 +0300

    Add Bmp confirmation and file size functions

--- ImageViewer/Bmp.cs
+++ ImageViewer/Bmp.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,27 +12,37 @@ namespace ImageViewer
 {
     class Bmp
     {
-        string Path;
-        string Name;
-        bool BmHeader;
+        public string Path { get; }
+        public string Name { get; }
+        byte[] bytes { get; }
+        public bool BmHeader { get; }
+        public int Size { get; }
         public Bmp(string path, string name)
         {
             Path = path;
             Name = name;
+            bytes = File.ReadAllBytes(path);
             BmHeader = GetConfirmation();
+            Size = GetFileSize();
         }
         public bool GetConfirmation()
         {
-            string head;
-            using (FileStream stream = new FileStream(Path, FileMode.Open, FileAccess.Read))
-            {
-                byte[] bytes = new byte[2];
-                stream.Read(bytes, 0, 2);
-                head = Encoding.Default.GetString(bytes);
-            }
-
+            string head = Encoding.Default.GetString(bytes, 0, 2);
             if (head == "BM") { return true; }
             else { return false; }
+        }
+        public int GetFileSize()
+        {
+            int size;
+            using (FileStream stream = new FileStream(Path, FileMode.Open, FileAccess.Read))
+            {
+                byte[] bytes = new byte[2 + 4];
+                stream.Read(bytes, 2, 4);
+                size = BitConverter.ToInt32(bytes, 0);
+            }
+            size = BitConverter.ToInt32(bytes, 2);
+            MessageBox.Show(size.ToString());
+            return size;
         }
     }
 }
