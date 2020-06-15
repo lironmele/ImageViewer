8f14c608df8ff612478108e75f2a41047ee31a93
Liron Meler <lironmeler@gmail.com>
2020-06-15 11:49:15 +0300

    Compact file size function

--- ImageViewer/Bmp.cs
+++ ImageViewer/Bmp.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
@@ -15,8 +16,6 @@ namespace ImageViewer
         public string Path { get; }
         public string Name { get; }
         byte[] bytes { get; }
-        public bool BmHeader { get; }
-        public int Size { get; }
         public Bmp(string path, string name)
         {
             Path = path;
@@ -29,18 +28,6 @@ namespace ImageViewer
             if (head == "BM") { return true; }
             else { return false; }
         }
-        public int GetFileSize()
-        {
-            int size;
-            using (FileStream stream = new FileStream(Path, FileMode.Open, FileAccess.Read))
-            {
-                byte[] bytes = new byte[2 + 4];
-                stream.Read(bytes, 2, 4);
-                size = BitConverter.ToInt32(bytes, 0);
-            }
-            size = BitConverter.ToInt32(bytes, 2);
-            MessageBox.Show(size.ToString());
-            return size;
-        }
+        public int GetFileSize() { return BitConverter.ToInt32(bytes, 2); }
     }
 }
