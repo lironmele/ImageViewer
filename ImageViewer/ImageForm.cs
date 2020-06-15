4c5344587081df6404a228e4f0c93fef79236a54
Liron Meler <lironmeler@gmail.com>
2020-06-15 14:14:31 +0300

    Add image form

---
+++ ImageViewer/ImageForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageViewer
+{
+    public partial class ImageForm : Form
+    {
+        Bitmap bmp;
+        public ImageForm(Bitmap bmp)
+        {
+            this.bmp = bmp;
+            InitializeComponent();
+        }
+
+        private void ImageForm_Load(object sender, EventArgs e)
+        {
+            Size = new Size(bmp.Width, bmp.Height);
+            picImage.Image = bmp;
+        }
+    }
+}
